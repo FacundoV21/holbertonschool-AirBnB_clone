4c52f9276f769cae2efba64293ec6e8c004aaaf3
Kratero <joseignaciomorenocamargo000@gmail.com>
2023-10-23 17:54:44 -0700

    BaseClass first test

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
 """
-
-
-
 """
 from uuid import uuid4
-import json
-
+from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid4())
-        """Buscar como usar datetime"""
-        self.created_at = "datetime"
-        self.updated_at = "datetime"
-    
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
     def __str__(self):
-        """
-            Deberia imprimir [<class name>] (<self.id>) <self.__dict__>
-        """
-        return f"[{__name__}] ({self.id}) {self.__dict__}"
+        """Deberia imprimir [<class name>] (<self.id>) <self.__dict__>"""
+        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
-        """
-            updates the public instance attribute updated_at with the current datetime
-        """
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-            returns a dictionary containing all keys/values of __dict__ of the instance
-        """
-    
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
