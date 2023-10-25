da0270fd4381ba9324f44ed449dca7d77a025526
Kratero <joseignaciomorenocamargo000@gmail.com>
2023-10-25 11:46:56 -0700

    Update FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""
+
+"""
 import json
 import sys
 
 class FileStorage:
-    __file_path = "file.json"
+    __file_path = "data.json"
     __objects = {}
 
     def all(self):
@@ -15,17 +18,10 @@ class FileStorage:
 
     def save(self):
         json_dictionary = {}
-        for k, obj in self.__objects.items:
-            json_dictionary[k] = obj.to_dict()
+        for k, v in self.__objects.items():
+            json_dictionary[k] = v.to_dict()
         with open(self.__file_path, "w", encoding="utf-8") as file:
             json.dump(json_dictionary, file)
 
     def reload(self):
-        try:
-            with open(self.__file_path, "r", encoding="utf-8") as file:
-                for k, v in json.load(file).items:
-                    clsobj = v.get(__class__)
-                      
-        except FileNotFoundError:
-            pass
- 
+        pass
