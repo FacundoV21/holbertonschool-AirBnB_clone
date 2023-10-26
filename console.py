066388d0c65857c346b2b1cb65aa50ca22c357fd
Kratero <joseignaciomorenocamargo000@gmail.com>
2023-10-26 07:46:16 -0700

    added command line interpreter with the help, quit and EOF commands

--- console.py
+++ console.py
@@ -5,6 +5,7 @@
     for writing line-oriented command interpreters.
 """
 import cmd
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -18,15 +19,15 @@ import sys
 class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
-    classes = [
-        "BaseModel",
-        "User",
-        "Place",
-        "Review",
-        "State",
-        "City",
-        "Amenity"
-    ]
+    classes = {
+        "BaseModel":  BaseModel,
+        "User": User,
+        "Place": Place,
+        "Review": Review,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity
+    }
 
     def do_EOF(self, line):
         return True
@@ -49,12 +50,30 @@ class HBNBCommand(cmd.Cmd):
         elif arg not in self.classes:
             print("** class doesn't exist **")
         else:
-            obj = eval(arg)()
-            obj.save()
+            obj = self.classes[arg]()
+            storage.new(obj)
+            storage.save()
             print(obj.id)
-            
+
     def help_create(self):
         print("Creates a new instace of an object\n")
+
+
+    def do_show(self, arg):
+        arguments = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif arguments[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(arguments) < 2:
+            print("** instance id missing **")
+        else:
+            key = f"{arguments[0]}.{arguments[1]}"
+            inst = storage.all()
+            if key in inst:
+                print(inst[key])
+            else:
+                print("** no instance found **")
 
     def help_show(self):
         print("Prints the string representation of an instance based on\
@@ -62,6 +81,25 @@ class HBNBCommand(cmd.Cmd):
 
     def help_destroy(self):
         print("Deletes an instance based on the class name and id\n")
+
+
+    def do_all(self, arg):
+        if arg and (arg not in self.classes):
+            print("** class doesn't exist **")
+        else:
+            inst = storage.all()
+            prt = []
+
+            if not arg:
+                for key, value in inst.items():
+                    #prt.append(str(inst[value]))
+                    print(str(inst[key]))
+            else:
+                for key, value in inst.items():
+                    if key.startswith(arg):
+                        prt.append(str(inst[key]))
+            print(prt)
+
 
     def help_all(self):
         print("Prints all string representation of all instances based\
