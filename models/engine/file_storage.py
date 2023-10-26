aad464dce20b53c15d26e636811052b35548bbd8
Kratero <joseignaciomorenocamargo000@gmail.com>
2023-10-26 07:46:59 -0700

    Update FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,7 @@
 import json
 import sys
 
+
 class FileStorage:
     __file_path = "data.json"
     __objects = {}
@@ -21,7 +22,30 @@ class FileStorage:
         for k, v in self.__objects.items():
             json_dictionary[k] = v.to_dict()
         with open(self.__file_path, "w", encoding="utf-8") as file:
-            json.dump(json_dictionary, file)
+            json.dump(json_dictionary, file, indent=4)
 
     def reload(self):
-        pass
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        classes = {
+        "BaseModel":  BaseModel,
+        "User": User,
+        "Place": Place,
+        "Review": Review,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity
+        }
+        try:
+            with open(self.__file_path, "r") as f:
+                objdict = json.load(f)
+                for k, v in objdict.items():
+                    self.new(classes[v["__class__"]](**v))
+
+        except FileNotFoundError:
+            pass
